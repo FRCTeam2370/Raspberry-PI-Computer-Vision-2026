9d5dd19e85977cc88e0ee7a3a8aacb55b306787a
Josh <joshuapankratz07@gmail.com>
2026-02-28 22:31:20 -0500

    lotta changes

--- IBOTS_fuel_detection.py
+++ IBOTS_fuel_detection.py
@@ -2,17 +2,18 @@ import cv2 # uv add open-cv
 from ultralytics import YOLO # uv add ultralytics
 import ntcore # uv add pyntcore https://robotpy.readthedocs.io/en/2023.5/install/pyntcore.html
 import math
-import time
 
 # ======
 # Setup Constants
 # ======
 MODEL_INPUT_SIZE = 352
-MODEL_DETECTION_CONF_THRESHOLD = 0.6
-CAMERA_FOV_HORIZONTAL = 0.6981   # CAMERA_FOV_HORIZONTAL should be in radians
-CAMERA_FOV_VERTICAL = 0.4363   # CAMERA_FOV_VERTICAL should be in radians
+MODEL_DETECTION_CONF_THRESHOLD = 0.4
+CAMERA_FOV_HORIZONTAL = 0.7892999999999791   # CAMERA_FOV_HORIZONTAL should be in radians
+CAMERA_FOV_VERTICAL = 0.49510000000001153   # CAMERA_FOV_VERTICAL should be in radians
+CAMERA_HORIZONTAL_OFFSET = 0.03718741
+CAMERA_VERTICAL_OFFSET = 0.02260995 
 CAMERA_INPUT_INDEX = 1
-MODEL_PATH = "/home/josh/Documents/ibots/2_16_26_last_full_integer_quant_edgetpu.tflite"
+MODEL_PATH = "/home/josh/Documents/ibots/2_17_26_90_full_integer_quant_edgetpu.tflite"
 DOUBLE_DETECTION_CLOSENESS_TOLERENCES = 0.01
 CLUMP_CLOSENESS_TOLERENCES = 0.35 # CLUMP_CLOSENESS_TOLERENCES should be in meters
 CLUMP_WEIGHT_MULTIPLIER = 1.3
@@ -80,12 +81,12 @@ while cap.isOpened():
         #print(results)
 
         boxes = results.boxes[results.boxes.conf > MODEL_DETECTION_CONF_THRESHOLD]
-        table.putNumber("number_of_fuel", boxes.__len__())
 
         # Collect Camera Field Position from Network Tables Format: x,y,rotation (rad)
         robot_position = [0.0,0.0,0.0]
+        intake_down = False
         robot_position = table.getEntry("Camera Pose").getDoubleArray(robot_position)
-        print(robot_position)
+        intake_down = table.getEntry("intakeDown").getBoolean(intake_down)
 
         final_boxes = []
         ball_positions_x = []       
@@ -105,11 +106,14 @@ while cap.isOpened():
             # Scale pixel position of bounding box to radians. Radians should be positive to the left and negative to the right
             yaw_radian = (-2*target_position_x/MODEL_INPUT_SIZE*CAMERA_FOV_HORIZONTAL)
             pitch_radian = (target_position_y/MODEL_INPUT_SIZE*CAMERA_FOV_VERTICAL)
+            pitch_radian = pitch_radian + CAMERA_VERTICAL_OFFSET
+            yaw_radian = yaw_radian + CAMERA_HORIZONTAL_OFFSET
 
             # Calculate distance based on bounding box lengths
             distance_height = MODEL_INPUT_SIZE/h * DIST_CONST_HEIGHT
             distance_width = MODEL_INPUT_SIZE/w * DIST_CONST_WIDTH
-            distance = round(distance_height, 2)
+            if distance_height > distance_width: distance = round(distance_height, 2)
+            else: distance = round(distance_width, 2)
             #print(f"Pitch: {pitch_radian}, Dist: {distance}")
 
             # Calculate ball position x,y
@@ -164,18 +168,27 @@ while cap.isOpened():
                 ball_positions_x.append(ball_position[0])
                 ball_positions_y.append(ball_position[1])
 
-        # Order by weight
-        if len(weights) > 0:
-            sortedValues = sorted(zip(weights, ball_positions_x, ball_positions_y), reverse=True)
-            weights, ball_positions_x, ball_positions_y = zip(*sortedValues)
-            print(weights)
-        table.putNumberArray("yaw_radians", yaw_radians)
-        table.putNumberArray("distance", distances)
-        table.putNumberArray("weights", weights)
-        table.putNumberArray("ball_position_x", ball_positions_x)
-        table.putNumberArray("ball_position_y", ball_positions_y)
+        if intake_down:
+            # Order by weight
+            if len(weights) > 0:
+                sortedValues = sorted(zip(weights, ball_positions_x, ball_positions_y), reverse=True)
+                weights, ball_positions_x, ball_positions_y = zip(*sortedValues)
 
-        # Show Feed
+            table.putNumber("number_of_fuel", len(weights))
+            table.putNumberArray("yaw_radians", yaw_radians)
+            table.putNumberArray("distance", distances)
+            table.putNumberArray("weights", weights)
+            table.putNumberArray("ball_position_x", ball_positions_x)
+            table.putNumberArray("ball_position_y", ball_positions_y)
+        else:
+            table.putNumber("number_of_fuel", 0)
+            table.putNumberArray("yaw_radians", [0])
+            table.putNumberArray("distance", [0])
+            table.putNumberArray("weights", [0])
+            table.putNumberArray("ball_position_x", [0])
+            table.putNumberArray("ball_position_y", [0])
+
+        '''# Show Feed
         results.boxes = final_boxes
         print(f"Filtered: {len(yaw_radians)}, Yaw Radians: {yaw_radians}, Weights: {weights}")
         annotatedFrame = results.plot()
@@ -189,7 +202,7 @@ while cap.isOpened():
         cv2.imshow("Processed Feed", annotatedFrame)
         if cv2.waitKey(1) == ord('q'):
             print("Exiting Program...")
-            break
+            break'''
 
 cap.release()
 cv2.destroyAllWindows()
